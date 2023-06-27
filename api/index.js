9278af70301295fff7b7e3be688806c567ecca40
Henry P B <mahe2di@gmail.com>
2023-06-26 21:18:56 -0500

    construyendo back1

--- api/index.js
+++ api/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const multer = require("multer");
+
 const app = express();
 const authRouter = require("./routes/auth");
 const usersRouter = require("./routes/users");
 const postsRouter = require("./routes/posts");
+const categoriesRouter = require("./routes/categories");
 
 dotenv.config();
 // app.use(bodyParser.json());
@@ -19,9 +22,24 @@ mongoose
   .then(console.log("Connected to MongoDB"))
   .catch((error) => console.log(error));
 
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+const upload = multer({ storage: storage });
+
+app.post("api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been uploaded");
+});
+
 app.use("/api/auth", authRouter);
 app.use("/api/users", usersRouter);
 app.use("/api/posts", postsRouter);
+app.use("/api/categories", categoriesRouter);
 
 app.listen("5000", () => {
   console.log("Backend runnig in port 5000");
