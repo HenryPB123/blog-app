42bd3722818dac4211969a2a6ba0fed7eb916799
Henry P B <mahe2di@gmail.com>
2023-06-26 20:13:26 -0500

    construyendo back

--- api/index.js
+++ api/index.js
@@ -2,9 +2,14 @@ const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const app = express();
+const authRouter = require("./routes/auth");
+const usersRouter = require("./routes/users");
+const postsRouter = require("./routes/posts");
 
 dotenv.config();
-
+// app.use(bodyParser.json());
+// app.use(cookieParser());
+app.use(express.json());
 mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
@@ -14,9 +19,9 @@ mongoose
   .then(console.log("Connected to MongoDB"))
   .catch((error) => console.log(error));
 
-app.use("/", (req, res) => {
-  console.log("This is your main URL");
-});
+app.use("/api/auth", authRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/posts", postsRouter);
 
 app.listen("5000", () => {
   console.log("Backend runnig in port 5000");
